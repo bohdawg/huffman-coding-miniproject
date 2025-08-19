cc41703cd712db996113ac804b88a7264efd879e
BohdanLeonidov <bohdan.leonidov.24@pnu.edu.ua>
2025-08-19 17:23:05 +0300

    Min-heap. Array of frequencies to array of nodes convertion

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #define CHAR_TABLE_SIZE 256
 
+struct Node {
+    unsigned char character;
+    unsigned int frequency;
+    struct Node* left;
+    struct Node* right;
+};//вузол мінімальної купи та дерева Гаффмана
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("Usage: %s [text file to convert]", argv[0]);
@@ -17,10 +24,12 @@ int main(int argc, char** argv) {
     unsigned int frequencies[CHAR_TABLE_SIZE] = {0};//ініціалізація масиву частот
     int c = 0;//ініціалізація символьного буферу
     while ((c = fgetc(finput)) != EOF) frequencies[(unsigned char)c]++;//посимвольне читтання файлу й збільшення індексів в масиві частот, чиє числове значення відповідає номеру символу в таблиці ASCII; додаткове виразне приведення типу
+    struct Node* tree_nodes[CHAR_TABLE_SIZE] = {0};//ініціалізація масиву вузлів мінімальної купи та дерева Гаффмана
     for (int i = 0; i < CHAR_TABLE_SIZE; i++) {
         if (frequencies[i] > 0) {
-            if ((unsigned char)i == '\n') printf("'\\n': %u\n", frequencies[i]);
-            else printf("'%c': %u\n", (unsigned char)i, frequencies[i]);
+            struct Node* newNode = (struct Node*)calloc(1, sizeof(struct Node));//використовую calloc для автоматичного призначення полям динамічної структури значення NULL
+            newNode->character = (unsigned char)i;
+            newNode->frequency = frequencies[i];
         }
     }
     fclose(finput);
